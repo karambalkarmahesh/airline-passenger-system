9d0ed94ce95c4a849a2e8a945886a25ca9b9bd2c
karambalkarmahesh <karambalkarmahesh@gmail.com>
2026-07-24 23:28:04 +0530

    added Swagger,mapstruct and exception handling with validation

--- passenger-service/src/main/java/com/airline/passengerservice/dto/PassengerRequestDTO.java
+++ passenger-service/src/main/java/com/airline/passengerservice/dto/PassengerRequestDTO.java
@@ -1,8 +1,7 @@
 package com.airline.passengerservice.dto;
 
 import com.airline.passengerservice.enums.Gender;
-import jakarta.validation.constraints.Email;
-import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.*;
 import lombok.*;
 
 import java.time.LocalDate;
@@ -19,11 +18,27 @@ public class PassengerRequestDTO {
     @NotBlank(message = "Last name is required")
     private String lastName;
 
+    @NotBlank(message = "Email is required")
     @Email(message = "Invalid email")
     private String email;
+
+    @NotBlank(message = "Phone number is required")
+    @Pattern(
+            regexp = "^[0-9]{10}$",
+            message = "Phone number must contain exactly 10 digits"
+    )
     private String phoneNumber;
+
+    @NotBlank(message = "Passport number is required")
     private String passportNumber;
+
+    @NotBlank(message = "Nationality is required")
     private String nationality;
+
+    @NotNull(message = "Date of birth is required")
+    @Past(message = "Date of birth must be in the past")
     private LocalDate dateOfBirth;
+
+    @NotNull(message = "Gender is required")
     private Gender gender;
 }
--- passenger-service/src/main/java/com/airline/passengerservice/mapper/PassengerMapper.java
+++ passenger-service/src/main/java/com/airline/passengerservice/mapper/PassengerMapper.java
@@ -3,7 +3,10 @@ package com.airline.passengerservice.mapper;
 import com.airline.passengerservice.dto.PassengerRequestDTO;
 import com.airline.passengerservice.dto.PassengerResponseDTO;
 import com.airline.passengerservice.entity.Passenger;
+import org.mapstruct.BeanMapping;
 import org.mapstruct.Mapper;
+import org.mapstruct.MappingTarget;
+import org.mapstruct.NullValuePropertyMappingStrategy;
 
 import java.util.List;
 
@@ -15,4 +18,10 @@ public interface PassengerMapper {
     PassengerResponseDTO toResponseDTO(Passenger passenger);
 
     List<PassengerResponseDTO> toResponseDTO(List<Passenger> passengers);
+
+    @BeanMapping(nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE)
+    void updatePassengerFromRequest(
+            PassengerRequestDTO request,
+            @MappingTarget Passenger passenger);
+
 }
